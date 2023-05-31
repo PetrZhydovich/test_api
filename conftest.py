5039183b05ce1cdfe2e96471d3ef679fb0e9ee32
Petr <povar_cr@mail.ru>
2023-05-31 18:44:18 +0300

    add Dz 3

--- conftest.py
+++ conftest.py
@@ -1,4 +1,27 @@
 import pytest
+import yaml
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
+
+with open("C:/Users/alexa/OneDrive/Рабочий стол/PageObject/testdata.yaml") as f:
+    testdata = yaml.safe_load(f)
+    browser = testdata["browser"]
+
+@pytest.fixture(scope='session')
+def browser():
+    if browser == "firefox":
+        service = Service(executable_path=GeckoDriverManager().install())
+        options = webdriver.FirefoxOptions()
+        driver = webdriver.Firefox(service=service, options=options)
+    else:
+        service = Service(executable_path=ChromeDriverManager().install())
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(service=service, options=options)
+    yield driver
+    driver.quit()
+
 
 @pytest.fixture()
 def x_selector1():
@@ -16,7 +39,6 @@ def x_selector3():
 def btn_selector():
     return "button"
 
- 
 @pytest.fixture()
 def result():
     return "401"
@@ -24,28 +46,3 @@ def result():
 @pytest.fixture()
 def hello_user():
     return """//*[@id="app"]/main/nav/ul/li[3]/a"""
-
-@pytest.fixture()
-def btn_create_post():
-    return """#create-btn"""
-
-@pytest.fixture()
-def enter_title():
-    return """//*[@id="create-item"]/div/div/div[1]/div/label/input"""
-
-@pytest.fixture()
-def enter_descr():
-    return """//*[@id="create-item"]/div/div/div[2]/div/label/span/textarea"""
-
-@pytest.fixture()
-def enter_content():
-    return """//*[@id="create-item"]/div/div/div[3]/div/label/span/textarea"""
-
-@pytest.fixture()
-def btn_save_post():
-    return "button > span"
-
-
-@pytest.fixture()
-def post_name():
-    return """//*[@id="app"]/main/div/div[1]/h1"""
--- test1.py
+++ test1.py
@@ -1,73 +1,28 @@
-import yaml
-from module import Site
+from testpage2 import OperationsHelper
+import logging
 import time
 
-with open("C:/Users/alexa/OneDrive/Рабочий стол/Selenium/Seminar/testdata.yaml") as f:
-    testdata = yaml.safe_load(f)
-site = Site(testdata["address"])
 
-# def test_step1(x_selector1, x_selector2, x_selector3, btn_selector, result):
-    
-#     input1 = site.find_element("xpath", x_selector1)
-#     input1.send_keys("test")
-    
-#     input2 = site.find_element("xpath", x_selector2)
-#     input2.send_keys("test")
-    
-#     btn = site.find_element("css", btn_selector)
-#     btn.click()
-    
-#     err_label = site.find_element("xpath", x_selector3)
-#     assert err_label.text == result
+"""Добавить в проект тест по проверке механики работы формы Contact Us на главной странице личного кабинета.
+ Должно проверятся открытие формы, ввод данных в поля, клик по кнопке и появление всплывающего alert.
+Совет: переключиться на alert можно командой alert = self.driver.switch_to.alert  Вывести текст alert.text """
 
-#     #site.close()
 
-def test_step2(x_selector1, x_selector2, btn_selector, hello_user):
+def test_step1(browser):
     
-    input1 = site.find_element("xpath", x_selector1)
-    input1.clear()
-    input1.send_keys(testdata['login'])
-    
-    input2 = site.find_element("xpath", x_selector2)
-    input2.clear()
-    input2.send_keys(testdata['passwd'])
-    
-    btn = site.find_element("css", btn_selector)
-    btn.click()
-
+    logging.info('Test Starting')
+    testpage = OperationsHelper(browser)
+    testpage.go_to_site()
+    testpage.enter_login('NatGlu')
+    testpage.enter_pass('db2d28bf9f')
+    testpage.click_login_button()
+    #assert testpage.get_error_text() == "401"
+    testpage.click_contact_button()
     time.sleep(3)
-    element = site.find_element("xpath", hello_user) 
-    assert element.text == f"Hello, {testdata['login']}"  
-        
-#     #site.close()
-
-"""Условие: Добавить в наш тестовый проект шаг добавления поста после входа.
- Должна выполняться проверка на наличие названия поста на странице сразу после его создания.
-Совет: не забудьте добавить небольшие ожидания перед и после нажатия кнопки создания поста. """
-
-def test_step3(btn_create_post, enter_title, enter_descr, enter_content, btn_save_post, post_name):
-    
-    btn_create_posts = site.find_element("css", btn_create_post)
-    btn_create_posts.click()
-
-    title = site.find_element("xpath", enter_title)
-    title.clear()
-    title.send_keys(testdata['title'])
-
-    description = site.find_element("xpath", enter_descr)
-    description.clear()
-    description.send_keys(testdata['description'])
-
-    content = site.find_element("xpath", enter_content)
-    content.clear()
-    content.send_keys(testdata['content'])
-
-    btn_save_posts = site.find_element("css", btn_save_post)
-    btn_save_posts.click()
-
+    testpage.enter_name('Glushko Natalya')
+    testpage.enter_email('test@mail.ru')
+    testpage.enter_content('Test information')
     time.sleep(3)
-    check_post = site.find_element("xpath", post_name) 
-    assert check_post.text == f"{testdata['title']}" 
-
-        
-    site.close()
+    testpage.click_contact_us_button()
+    time.sleep(3)
+    assert testpage.alert() == 'Form successfully submitted'
